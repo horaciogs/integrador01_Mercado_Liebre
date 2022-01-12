c6f02a0c1d08b8b7cf118c224ddfe2791c468a15
horaciogs <hgs.linux@gmail.com>
2022-01-12 15:59:48 -0300

    version 2 responsive

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-//requieres de path y express
+//solicito el la función express y lo guardo en express
 const express = require ('express');
-const path = require('path');
+//solicito la funcion path
+const path = require ('path');
 
-//se llama a la función de express y guardo los objetos en app
+//guardo los objetos de express en app para ser accesibles
 const app = express();
+//guardo el path absoluto de public
+const publicPath = path.resolve(__dirname, "./public");
 
-//indico a express mi carpeta con contenido estático (public)
-publicPath = path.resolve(__dirname, './public');
+//le digo a express que la carpeta public será estática
 app.use(express.static(publicPath));
 
-//inicializo la escucha en el puerto 3000
-app.listen(3000, () => {
+//abro la conexión con el servidor en el puerto 3000
+app.listen(3000, ()=> {
     console.log("Servidor corriendo en el puerto 3000");
 });
 
-//respuesta a get para /home
-app.get('/home', function(req,res){
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+//por método get envío el archivo .html
+app.get('/', function (req,resp) {
+    resp.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get('/register', function (req,resp) {
+    resp.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+
+app.get('/login', function (req,resp) {
+    resp.sendFile(path.resolve(__dirname, './views/login.html'))
 });
